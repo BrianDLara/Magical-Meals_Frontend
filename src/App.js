80f8f708b1fd9266a9c87eb6df73dac16267959b
Brian Lara <Briandlara1@gmail.com>
2022-12-07 23:46:04 -0500

    created a tailwind navbar with css transitions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './index.css'
 import { Routes, Route } from 'react-router-dom'
 
 import Test from './components/TestComponent'
-
+import Nav from './components/Nav'
 function App() {
   return (
-    <div>
-      <Test />
+    <div className="App">
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Test />} />
+        </Routes>
+      </main>
     </div>
   )
 }
