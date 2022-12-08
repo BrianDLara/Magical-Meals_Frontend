bfaf1849fdff979d46111dbd9b887e4afdcfb52e
Brian Lara <Briandlara1@gmail.com>
2022-12-07 22:27:45 -0500

    updated package.json, import browser router to index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './index.css';
-import Test from './components/TestComponent';
+import './index.css'
+import { Routes, Route } from 'react-router-dom'
+
+import Test from './components/TestComponent'
 
 function App() {
   return (
     <div>
       <Test />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
