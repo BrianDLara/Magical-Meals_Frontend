1bf261fc4666b38ebaf0124e626f6e8a55a4231b
Brian Lara <Briandlara1@gmail.com>
2022-12-12 16:20:02 -0500

    prepared a kroger button to set logic

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -27,6 +27,10 @@ const Cart = () => {
     
     return (
     <div className='min-h-screen text-white py-4 pb-24 sm:py-12'>
+        <div className='flex justify-center pt-6'>
+        <span> <button className="font-2-bold text-xl focus:outline-none text-white bg-amber-500 hover:bg-amber-500 focus:ring-4 focus:ring-amber-300 font-medium rounded-lg text-sm px-8 py-2.5 mr-2 mb-2 dark:focus:ring-amber-900">Purchase At <img src="https://i.imgur.com/aZSOcMJ.png" alt="kroger" className='kroger-image' /></button></span>
+        </div>
+        
         <div className='sm:grid grid-cols-2 gap-1'>
             {cartItems.map((item) => (
                 <div className='cart-data-container' key={item.id}>
