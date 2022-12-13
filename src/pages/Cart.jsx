0b3981876fe652329e7b7958f6d24c32faf9e056
Brian Lara <Briandlara1@gmail.com>
2022-12-13 12:11:04 -0500

    fixed paypal z-index issue

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 import {useState, useEffect, useCallback} from 'react'
 import { useParams } from 'react-router-dom'
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import {loadStripe} from '@stripe/stripe-js';
+
 const CLIENT_ID = process.env.REACT_APP_PAYPAL_CLIENT_ID
-console.log(CLIENT_ID)
+const CLIENT_SECRET = process.env.REACT_APP_STRIPE_KEY
+const PUBLISHABLE_KEY = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+
 
 
 const Cart = () => {
@@ -56,45 +60,50 @@ const Cart = () => {
         handleRefresh()
     }
 
+
+    // paypal
     const initialOptions = {
         "client-id": CLIENT_ID,
         currency: "USD",
         intent: "capture",
     };
 
+    // stripe
 
-    return (
+    const stripePromise = loadStripe(`${PUBLISHABLE_KEY}`);
+
+
+    return cartItems !== null ? (
     <div className='min-h-screen text-white py-4 pb-24 sm:py-12'>
         {/* <h1>Hi {userInfo.name}</h1> */}
-        <div className='flex justify-center pt-6'>
+        <div className='flex flex-col text-center justify-center pt-6'>
             <p className='text-2xl font-1-bold'>Total: &nbsp;${total}</p>
-        </div>
-        <PayPalScriptProvider options={initialOptions}>
-        <PayPalButtons
-                createOrder={(data, actions) => {
-                    return actions.order.create({
-                        purchase_units: [
-                            {
-                                amount: {
-                                    value: `${total}`
-                                },
-                            },
-                        ],
-                    });
-                }}
-                onApprove={(data, actions) => {
-                    return actions.order.capture().then((details) => {
-                        const name = details.payer.name.given_name;
-                        alert(`Transaction completed by ${name}`);
-                    });
-                }}
-            />
-        </PayPalScriptProvider>
-        <div className='flex justify-center pt-6'>
-            <span> <button className="font-2-bold text-xl focus:outline-none text-white bg-amber-500 hover:bg-amber-500 focus:ring-4 focus:ring-amber-300 font-medium rounded-lg text-sm px-8 py-2.5 mr-2 mb-2 dark:focus:ring-amber-900">Purchase At <img src="https://i.imgur.com/aZSOcMJ.png" alt="kroger" className='kroger-image' /></button></span>
+            <div className='flex items-center justify-center pt-4'>
+                <PayPalScriptProvider options={initialOptions}>
+                    <PayPalButtons
+                        createOrder={(data, actions) => {
+                            return actions.order.create({
+                                purchase_units: [
+                                    {
+                                        amount: {
+                                            value: `${total}`
+                                        },
+                                    },
+                                ],
+                            });
+                        }}
+                        onApprove={(data, actions) => {
+                            return actions.order.capture().then((details) => {
+                                const name = details.payer.name.given_name;
+                                alert(`Transaction completed by ${name}`);
+                            });
+                        }}
+                    />
+                </PayPalScriptProvider>
+            </div>
         </div>
         
-        <div className='sm:grid grid-cols-2 gap-1'>
+        <div className='sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1'>
             {cartItems.map((item) => (
                 <div className='cart-data-container' key={item.id}>
                     <div className='mx-10 flex justify-between items-center'>
@@ -102,11 +111,14 @@ const Cart = () => {
                         <button onClick={() => handleDelete(item.id)} className="font-2-bold focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 rounded-lg text-sm px-3 py-2.5 mr-3 mb-4 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Remove</button>
                     </div>
                     <img className="cart-image" src={item.image} alt={item.name} width='50px'/>
+                    <p>amount: {item.amount}</p>
                 </div>
             ))}
         </div> 
     </div>
-  )
+  ) :     
+   null
+
 }
 
 export default Cart
